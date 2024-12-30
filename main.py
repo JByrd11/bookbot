827e1073b83b88b1af87183a417780396693f2e1
JByrd11 <jtbyrd1196@gmail.com>
2024-12-30 13:00:47 -0500

    Completed bookbot

---
+++ main.py
@@ -0,0 +1,57 @@
+def get_text_from_file(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(string):
+    words = string.split()
+    return len(words)
+
+def character_count(string):
+    lowered = string.lower()
+    char_counts = {}
+    for char in lowered:
+        if char.isalpha():
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
+    return char_counts
+
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(filename, word_count, char_list):
+    print(f"--- Begin report of {filename} ---")
+    print(f"{word_count} words found in the document")  # Changed this line
+    print("")
+    for i in char_list:
+        print(f"The '{i['char']}' character was found {i['count']} times")
+    print("")
+    print("--- End report ---")
+
+# Main execution
+text = get_text_from_file("books/frankenstein.txt")
+word_count_result = word_count(text)
+char_counts = character_count(text)
+
+char_list = []
+for char, count in char_counts.items():
+    char_dict = {"char": char, "count": count}
+    char_list.append(char_dict)
+
+char_list.sort(reverse=True, key=sort_on)
+print_report("books/frankenstein.txt", word_count_result, char_list)
+
+
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        
+       
+
+if __name__ == "__main__":
+    main()
+
+
+
